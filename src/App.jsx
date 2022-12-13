b6d53e271edcac1ee1d93465dadbb351b217fd45
LittleLlama246 <114889233+LittleLlama246@users.noreply.github.com>
2022-12-12 23:13:25 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar";
-import { HashRouter as Router, Routes, Route, Navigate,} from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 //main pages
 import About from './components/About';
